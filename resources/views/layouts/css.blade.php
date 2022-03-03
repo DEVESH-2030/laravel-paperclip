405512590a3f1d2ef76154021d3122326d87f2b9
devesh <bit.devesh2030@gmail.com>
2022-03-03 11:56:43 +0530

    Removed commented codes

--- resources/views/layouts/css.blade.php
+++ resources/views/layouts/css.blade.php
@@ -5,5 +5,3 @@
         <link rel="stylesheet" type="text/css" href="{{ asset('css/nunito_falimy.css') }}">
         <link rel="stylesheet" type="text/css" href="path/to/font-awesome/css/font-awesome.min.css">
         @toastr_css
-        {{-- <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet"> --}}
-        {{-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous"> --}}
